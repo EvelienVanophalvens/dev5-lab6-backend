2d90f0dcd94587c9a363b2caf2820fc520b1ee37
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2023-11-26 09:27:03 +0100

    Feat: send date to client

--- primus/live.js
+++ primus/live.js
@@ -4,6 +4,16 @@ module.exports.go = (server) => {
    //check if connection, then console.log
    primus.on('connection', (spark) => {
        console.log('connected');
+
+        //check if data is received, then console.log
+        spark.on('data', (data) => {
+            console.log(data, 'data received');
+            //send data back to client
+            spark.write(data);
+        });
+
+
    });
+
 
 }
